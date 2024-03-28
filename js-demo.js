86a422b7c72ef4e80c917928d5b25e651f691e84
pajuelab <165089084+pajuelab@users.noreply.github.com>
2024-03-28 14:58:32 -0700

    Add files via upload

---
+++ js-demo.js
@@ -0,0 +1,28 @@
+﻿/*
+   JS Activity Clock
+   Name: Paola Barrera
+   Date:   3/26/24
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock (){
+   var thisDay = new Date();
+   var thisDate = thisDay.toLocaleDateString();
+   var thisDayNum = thisDay.getDay();
+   var thisWeekday = getWeekday(thisDayNum);
+   var thisTime = thisDay.toLocaleTimeString();
+
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime; 
+}
+
+function getWeekday(dayNum){
+   var wDays = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+   return wDays [dayNum];
+}
